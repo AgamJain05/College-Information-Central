b31dab075328803b893dd5d07963c08def091de9
Agam Jain <jainagam4@gmail.com>
2024-07-07 23:43:47 +0530

    removed extra signin route

--- src/index.js
+++ src/index.js
@@ -2,21 +2,28 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import {app} from "./app.js";
+import express from 'express';
 dotenv.config({
     path: './.env'
 })
 
 
 
+// const server = express()
+
+
+
+
+
+// server.use(express.json())
 connectDB()
-server.post('/signin', (req, res) => {
-    res.json(req.body)
-  }
-)
+
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
+      
+    
 })
 .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
