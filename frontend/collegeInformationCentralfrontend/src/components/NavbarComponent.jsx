0c525d2f6deb6ebc65a0bf9427dfa2e76050a83d
Agam Jain <jainagam4@gmail.com>
2024-07-23 23:48:19 +0530

    chore: Update logo based on theme in NavbarComponent.jsx

--- frontend/collegeInformationCentralfrontend/src/components/NavbarComponent.jsx
+++ frontend/collegeInformationCentralfrontend/src/components/NavbarComponent.jsx
@@ -4,14 +4,16 @@ import { Link, Outlet, useNavigate  } from "react-router-dom";
 import logo from "../imgs/logo.png";
 import { Usercontext } from '../App';
 import UserNavigationPanel from "./user-navigation.component";
-
+import darkLogo from "../imgs/logo-dark.png";
+import lightLogo from "../imgs/logo-light.png";
+import { ThemeContext} from '../App';
 const Navbar = () => {
     const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
     const [userNavPanel, setUserNavPanel] = useState(false);
     const { userAuth, userAuth: { access_token, profile_img } } = useContext(Usercontext);
 
     let navigate = useNavigate();
-
+    let { theme, setTheme } = useContext(ThemeContext);
     const handleUserNavPanel = () => {
         setUserNavPanel(currentVal => !currentVal);
     }
@@ -27,11 +29,23 @@ const Navbar = () => {
             navigate(`/search/${query}`);
         }
     }
+
+    const changeTheme = () => {
+        
+        let newTheme = theme == "light" ? "dark" : "light";
+
+        setTheme(newTheme);
+
+        document.body.setAttribute("data-theme", newTheme);
+
+        storeInSession("theme", newTheme);
+
+    }
     return (
         <>
             <nav className="navbar">
-                <Link to="/" className="flex-none w-10">
-                    <img src={logo} className="w-full" />
+            <Link to="/" className="flex-none w-10">
+                    <img src={ theme == "light" ? darkLogo : lightLogo } className="w-full" />
                 </Link>
                 <div className={`absolute bg-white w-full left-0 top-full mt-0.5 border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show ${searchBoxVisibility ? "show" : "hide"} `} >
                     <input type="text" placeholder="Search" className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12" onKeyDown ={handleSearchBox}/>
@@ -46,10 +60,14 @@ const Navbar = () => {
                         <i className="fi fi-rr-file-edit"></i>
                         <p>Write</p>
                     </Link>
+
+                    <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10" onClick={changeTheme}>
+                        <i className={"fi fi-rr-" + ( theme == "light" ?  "moon-stars" : "sun" ) + " text-2xl block mt-1" }></i>
+                    </button>
                     {
                         access_token ?
                             <>
-                                <Link to="/dashboard/notification">
+                                <Link to="/dashboard/notifications">
                                     <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
                                         <i className="fi fi-rr-bell text-2x1 block mt-1"></i>
                                     </button>
