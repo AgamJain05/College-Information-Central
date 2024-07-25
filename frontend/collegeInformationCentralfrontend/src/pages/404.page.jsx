e39f8e393963778a23dae1e5883be62ab76248b6
Agam Jain <jainagam4@gmail.com>
2024-07-25 23:40:29 +0530

    chore: Update 404 page with theme-based images and logos

--- frontend/collegeInformationCentralfrontend/src/pages/404.page.jsx
+++ frontend/collegeInformationCentralfrontend/src/pages/404.page.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
-import pageNotFoundImage from "../imgs/404.png";
-import fullLogo from "../imgs/full-logo.png";
-
+import { useContext } from "react";
+import lightPageNotFoundImg from "../imgs/404-light.png";
+import darkPageNotFoundImg from "../imgs/404-dark.png";
+import lightFullLogo from "../imgs/full-logo-light.png";
+import darkFullLogo from "../imgs/full-logo-dark.png";
+import { ThemeContext } from "../App";
 const PageNotFound = () => {
+
+    let { theme } = useContext(ThemeContext);
+
     return (
         <section className="h-cover relative p-10 flex flex-col items-center gap-20 text-center">
-            <img src={pageNotFoundImage} className="select-none border-2 border-grey w-72 aspect-square object-cover rounded" />
+            <img src={ theme == "light" ? darkPageNotFoundImg : lightPageNotFoundImg } className="select-none border-2 border-grey w-72 aspect-square object-cover rounded" />
             <h1 className="text-4xl font-gelasio leading-7">Page not found</h1>
             <p className="text-dark-grey text-xl leading-7 -mt-8">The page you are looking for does not exist
             <br />
@@ -15,7 +21,7 @@ const PageNotFound = () => {
                 </Link>
             </p>
             <div className="mt-auto">
-                <img src={fullLogo} className="h-8 object-contain block mx-auto select-none" />
+                <img src={ theme == "light" ? darkFullLogo : lightFullLogo } className="h-8 object-contain block mx-auto select-none" />
                 <p className="mt-5 text-dark-grey">Read millions of stories around the college</p>
             </div>
         </section>
